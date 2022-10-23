f1edba2e7a016366eef0499b48c6a1573b65d73a
Ahmed ben hmida <79115867+AhmedBenHamida@users.noreply.github.com>
2022-10-23 15:46:33 +0100

    Update Application.java

--- src/main/java/com/example/esprit/micro/project/Application.java
+++ src/main/java/com/example/esprit/micro/project/Application.java
@@ -2,12 +2,14 @@ package com.example.esprit.micro.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.stereotype.Component;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 @Component
 @SpringBootApplication
 @EnableEncryptableProperties
+@EnableEurekaClient
 public class Application {
 
 	public static void main(String[] args) {
